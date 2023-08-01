5f100c474f19e17509f72aee4f599d5f283ec0ff
L8duck <ndkhoa1000@gmail.com>
2023-08-02 06:57:11 +0700

    add bcrypt-nodejs

--- server.js
+++ server.js
@@ -1,11 +1,57 @@
-import express from 'express';
+import express, { response } from 'express';
 import fs from 'fs';
 
 const app = express();
-
+app.use(express.json());
+const database ={
+    user: [
+        {
+            id: '123',
+            name:'Khoa',
+            email:'khoa@gmail.com',
+            password:'biscuit',
+            entries: 0,
+            joined: new Date()
+        },
+        {
+            id: '124',
+            name:'Than',
+            email:'than@gmail.com',
+            password:'donut',
+            entries: 0,
+            joined: new Date()
+        }
+    ]
+}
 app.get('/', (req,res)=>{
-    res.send('success at root');
+    res.json(database.user);
+})
+
+app.post('/signin',(req,res) => {
+    if(req.body.email === database.user[0].email ){
+        res.json("success");
+    } else res.status(400).json("fail");
+})
+
+app.post('/register',(req,res) => {
+    const {name, email, password} = req.body;
+    database.user.push({
+        id: '125',
+            name:name,
+            email: email,
+            password: password,
+            entries: 0,
+            joined: new Date()
+    });
+    res.json(database.user[database.user.length-1]);
 })
 
 app.listen(3000, () =>{
 });
+/*
+    /signin --> POST: success/fail
+    /register --> POST: user
+    /profile/userID --> GET = user => rank
+    /image --> PUT = user
+
+*/
